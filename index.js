37945b61decdce36054c67413aabefd1800cde50
Alsew <aurelien@hemidy.fr>
2022-06-05 13:40:44 +0200

    :sparkles: Added seconds lines and pourcentage on hours based on minutes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import GUI from 'https://cdn.jsdelivr.net/npm/lil-gui@0.16/+esm';
+import GUI from "https://cdn.jsdelivr.net/npm/lil-gui@0.16/+esm";
 
 const parameters = {
   hours: 1,
@@ -8,13 +8,20 @@ const parameters = {
 
 const gui = new GUI();
 gui
-  .add(parameters, 'minutes')
+  .add(parameters, "minutes")
   .min(0)
   .max(360)
   .onChange((value) => {
     console.log(value);
-    document.querySelector('.seconds-needle').style.transform = `translate(0, calc(-100% + 5px)) rotate(${value}deg)`;
+    document.querySelector(
+      ".seconds-needle"
+    ).style.transform = `translate(0, calc(-100% + 5px)) rotate(${value}deg)`;
   });
+
+const radiansToDegree = (radians) => {
+  let pi = Math.PI;
+  return radians * (180 / pi);
+};
 
 class Clock {
   constructor() {
@@ -35,49 +42,53 @@ class Clock {
     };
 
     for (let i = 1; i < hours + 1; i++) {
-      const hour = document.createElement('div');
+      const hour = document.createElement("div");
       hour.classList.add(`hour-number`);
       hour.classList.add(`hour-number-${i}`);
       hour.innerText = i;
-      document.querySelector('.clock').appendChild(hour);
+      document.querySelector(".clock").appendChild(hour);
 
       let x = radius * Math.cos((Math.PI / 6) * i - Math.PI / 2) + center.x;
       let y = radius * Math.sin((Math.PI / 6) * i - Math.PI / 2) + center.y;
       hour.style.left = `${x}px`;
       hour.style.top = `${y}px`;
-      hour.style.transform = 'translate(-50%, -50%)';
+      hour.style.transform = "translate(-50%, -50%)";
 
-      const pointHour = document.createElement('div');
+      const pointHour = document.createElement("div");
       pointHour.classList.add(`hour-point`);
       pointHour.classList.add(`hour-point-${i}`);
-      document.querySelector('.clock').appendChild(pointHour);
+      document.querySelector(".clock").appendChild(pointHour);
 
-      let xPoint = (radius + 20) * Math.cos((Math.PI / 6) * i - Math.PI / 2) + center.x;
-      let yPoint = (radius + 20) * Math.sin((Math.PI / 6) * i - Math.PI / 2) + center.y;
+      let xPoint =
+        (radius + 20) * Math.cos((Math.PI / 6) * i - Math.PI / 2) + center.x;
+      let yPoint =
+        (radius + 20) * Math.sin((Math.PI / 6) * i - Math.PI / 2) + center.y;
       pointHour.style.left = `${xPoint}px`;
       pointHour.style.top = `${yPoint}px`;
-      pointHour.style.transform = 'translate(-50%, -50%)';
+      pointHour.style.transform = "translate(-50%, -50%)";
     }
   }
 
   setSecondsOnClock() {
     const seconds = 60;
-    const radius = 350 / 2;
+    const radius = 345 / 2;
     const center = {
       x: 350 / 2,
       y: 350 / 2,
     };
 
     for (let i = 1; i < seconds + 1; i++) {
-      const seconds = document.createElement('div');
+      const seconds = document.createElement("div");
       seconds.classList.add(`seconds-tick`);
-      document.querySelector('.clock').appendChild(seconds);
+      document.querySelector(".clock").appendChild(seconds);
 
       let x = radius * Math.cos((Math.PI / 30) * i) + center.x;
       let y = radius * Math.sin((Math.PI / 30) * i) + center.y;
       seconds.style.left = `${x}px`;
       seconds.style.top = `${y}px`;
-      seconds.style.transform = 'translate(-50%, -50%)';
+      seconds.style.transform = `translate(-50%, -50%) rotate(${
+        radiansToDegree((Math.PI / 30) * i) - 90
+      }deg)`;
     }
   }
 
@@ -89,21 +100,29 @@ class Clock {
 
   updateHours() {
     let hours = new Date().getHours();
-    document.querySelector('.hours-needle').style.transform = `translate(0, calc(-100% + 3.25px)) rotate(${
-      (hours * 360) / 12
+    let minutes = new Date().getMinutes();
+    let hourPercent = ((Math.PI / 6) * minutes) / 60;
+    document.querySelector(
+      ".hours-needle"
+    ).style.transform = `translate(-50%, -100%) rotate(${
+      (hours * 360) / 12 + radiansToDegree(hourPercent)
     }deg)`;
   }
 
   updateMinutes() {
     let minutes = new Date().getMinutes();
-    document.querySelector('.minutes-needle').style.transform = `translate(0, calc(-100% + 3.25px)) rotate(${
+    document.querySelector(
+      ".minutes-needle"
+    ).style.transform = `translate(-50%, -100%) rotate(${
       (minutes * 360) / 60
     }deg)`;
   }
 
   updateSeconds() {
     let seconds = new Date().getSeconds();
-    document.querySelector('.seconds-needle').style.transform = `translate(0, calc(-100% + 1.25px)) rotate(${
+    document.querySelector(
+      ".seconds-needle"
+    ).style.transform = `translate(-50%, -100%) rotate(${
       (seconds * 360) / 60
     }deg)`;
   }
